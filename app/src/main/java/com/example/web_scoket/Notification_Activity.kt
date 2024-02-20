9900ee87169405e12686bb2bc14266808b520217
guddu <guddulawaniya123@gmail.com>
2024-02-20 22:30:50 +0530

    web socket implement with notification

---
+++ app/src/main/java/com/example/web_scoket/Notification_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.web_scoket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notification_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
